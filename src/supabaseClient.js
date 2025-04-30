6aeb5180f1a2b7fc4c05a4c037828060a5db8da8
Sanjay9729 <sanjay.vaghela@appunik.com>
2025-04-30 12:56:48 +0530

    supabase updated

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,6 +3,12 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://uonsrhcedftrjoduwivq.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVvbnNyaGNlZGZ0cmpvZHV3aXZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ2Mjg2NjYsImV4cCI6MjA2MDIwNDY2Nn0.iwjL1pjIXfFgb8nOqGFn8UrDskhQIeyiYLqgV1Eh0AA'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
-
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    auth: {
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: true,
+      flowType: 'pkce',
+      debug: true
+    }
+  });
