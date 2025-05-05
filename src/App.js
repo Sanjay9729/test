741f1aa082ffbaca53cd53c8e980245f28dc4c81
Sanjay9729 <sanjay.vaghela@appunik.com>
2025-05-05 18:39:04 +0530

    path chang

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Auth from './pages/Auth';
-import Home from './pages/Home';
-import ViewWarranty from './pages/ViewWarranty';
+// src/App.js
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Auth from './pages/Auth'
+import Home from './pages/Home'
+import ViewWarranty from './pages/ViewWarranty'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Auth />} />
-        <Route path="/" element={<ViewWarranty />} />     
-        <Route path="/home" element={<Home />} />        
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<ViewWarranty />} />
       </Routes>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
+
+
+
