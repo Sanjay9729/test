3c1845add788d6df5aa2489a4e881690dfcfab5d
Sanjay9729 <sanjay.vaghela@appunik.com>
2025-05-06 16:07:01 +0530

    update app js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Auth from './pages/Auth';
 import Home from './pages/Home';
 import ViewWarranty from './pages/ViewWarranty';
 
 function App() {
-  const host = new URLSearchParams(window.location.search).get("host");
-
-  const config = {
-    apiKey: "43db155c41abc41f085ba33378e97fa0", 
-    host: host,
-    forceRedirect: true,
-  };
-
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Auth />} />
-        <Route path="/" element={<ViewWarranty />} />     
-        <Route path="/home" element={<Home />} />        
+        <Route path="/" element={<ViewWarranty />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
