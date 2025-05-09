5b5c3e4a222795d795f2271a92be14c6cf87b7a3
Sanjay9729 <sanjay.vaghela@appunik.com>
2025-05-09 11:43:48 +0530

    added the polaris css

--- src/pages/ViewWarranty.jsx
+++ src/pages/ViewWarranty.jsx
@@ -175,6 +175,8 @@ import {
   Text,
 } from '@shopify/polaris';
 import enTranslations from '@shopify/polaris/locales/en.json';
+import '@shopify/polaris/build/esm/styles.css';
+
 
 const ViewWarranty = () => {
   const [submissions, setSubmissions] = useState([]);
