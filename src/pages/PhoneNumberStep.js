f93bae8ea21534bbccf45de48319fb5b962aab0f
Sanjay9729 <sanjay.vaghela@appunik.com>
2025-06-02 17:36:44 +0530

    update product sku

--- src/pages/PhoneNumberStep.js
+++ src/pages/PhoneNumberStep.js
@@ -39,7 +39,7 @@ const PhoneNumberStep = ({ phone, setPhone, nextStep, fieldErrors }) => {
     <section className="step-section active slide-up">
       <div className="step-label">
       <div className="step_number_main">
-              <span className="step-number">3</span>
+              <span className="step-number">4</span>
               <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" viewBox="0 0 16 16" class="shouldFlipIfRtl">
   <path fill-rule="evenodd" clip-rule="evenodd" d="M8.47 1.97a.75.75 0 0 1 1.06 0l4.897 4.896a1.25 1.25 0 0 1 0 1.768L9.53 13.53a.75.75 0 0 1-1.06-1.06l3.97-3.97H1.75a.75.75 0 1 1 0-1.5h10.69L8.47 3.03a.75.75 0 0 1 0-1.06Z"></path>
 </svg>
